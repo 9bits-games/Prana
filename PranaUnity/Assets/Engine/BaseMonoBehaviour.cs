ce2ed1b3c73981d4a9cdf11c647aebdde4e0ecdb
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-12-02 15:23:24 -0500

    Project structure

---
+++ PranaUnity/Assets/Engine/BaseMonoBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Wraper base class of MonoBehaviour, all MonoBehaviour classes
+ * must inherit from BaseMonoBehaviour instead of MonoBehaviour.
+ **/
+public class BaseMonoBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+
+	// Update is called once per frame
+	void Update () {}
+}
---
+++ PranaUnity/Assets/Engine/SceneController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * The base class of all SceneControllers.
+ **/
+public class SceneController : BaseMonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+
+	// Update is called once per frame
+	void Update () {}
+}
