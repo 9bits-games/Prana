cc627ac1a379c7ea3bd2bc07d2a2b0a3ec42a977
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-12-25 12:10:20 -0500

    Add: Advanced Terrain Shader V2

---
+++ PranaUnity/Assets/GameScenes/Common/Scripts/Cinematic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cinematic: BaseMonoBehaviour {
+    private float time;
+
+    public bool paused = false;
+
+    void Start () {
+        time = 0;
+    }
+
+    void Update() {
+        if (!paused) {
+//            time 
+        }
+    }
+}
