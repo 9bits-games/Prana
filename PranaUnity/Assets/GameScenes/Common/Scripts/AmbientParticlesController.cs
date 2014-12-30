0a7474e953488de452ac7b430753e027aa117914
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-12-29 19:57:40 -0500

    Add: Ambient Particles

---
+++ PranaUnity/Assets/GameScenes/Common/Scripts/AmbientParticlesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class AmbientParticlesController : BaseMonoBehaviour {
+    public Transform TargetToFollow;
+
+    void Awake() {
+    }
+
+    void Update() {
+        transform.position = TargetToFollow.position;
+    }
+}
+
